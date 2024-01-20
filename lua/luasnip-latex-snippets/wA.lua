01aad319c0e8d0772ae6c1948b60acc3c2b2c964
yunus <yusun@smail.nju.edu.cn>
2024-01-20 15:44:33 +0800

    add dm

--- lua/luasnip-latex-snippets/wA.lua
+++ lua/luasnip-latex-snippets/wA.lua
@@ -10,8 +10,9 @@ function M.retrieve(not_math)
   }) --[[@as function]]
 
   return {
-    parse_snippet({ trig = "mk", name = "Math" }, "\\( ${1:${TM_SELECTED_TEXT}} \\)$0"),
-    parse_snippet({ trig = "dm", name = "Block Math" }, "\\[\n\t${1:${TM_SELECTED_TEXT}}\n.\\] $0"),
+    -- parse_snippet({ trig = "mk", name = "Math" }, "\\( ${1:${TM_SELECTED_TEXT}} \\)$0"),
+    -- parse_snippet({ trig = "dm", name = "Block Math" }, "\\[\n\t${1:${TM_SELECTED_TEXT}}\n.\\] $0"),
+    parse_snippet({ trig = "dm", name = "Math" }, "$${1:${TM_SELECTED_TEXT}}$ $0"),
   }
 end
 
